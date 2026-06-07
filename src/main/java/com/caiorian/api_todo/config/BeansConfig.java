11a386acd753f22283829120d9499f5a9260b90f
CaioRian187 <caiorian246810@gmail.com>
2026-06-07 11:30:10 -0300

    adicionando o bean para gerenciar o TodoService

--- src/main/java/com/caiorian/api_todo/config/BeansConfig.java
+++ src/main/java/com/caiorian/api_todo/config/BeansConfig.java
@@ -1,8 +1,12 @@
 package com.caiorian.api_todo.config;
 
+import com.caiorian.api_todo.domain.ports.TodoRepositoryPort;
+import com.caiorian.api_todo.domain.services.TodoService;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class BeansConfig {
 
     @Bean
@@ -10,4 +14,8 @@ public class BeansConfig {
         return new ModelMapper();
     }
 
+    @Bean
+    public TodoService todoService(TodoRepositoryPort todoRepositoryPort){
+        return new TodoService(todoRepositoryPort);
+    }
 }
